dc22a1ed7def39b8eff5a357a1d6fd22cd30cf3c
Petri Mattila <Etherkon@users.noreply.github.com>
2022-03-05 14:09:30 +0200

    Create mock-tasks.ts

---
+++ app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Ohjelmistosuunnittelua',
+    day: 'Maaliskuu 7. kello 9:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Työtapaaminen',
+    day: 'Maaliskuu 8. kello 14:00',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Osien hankinta',
+    day: 'Maaliskuu 8. kello 16:00',
+    reminder: false,
+  },
+];
